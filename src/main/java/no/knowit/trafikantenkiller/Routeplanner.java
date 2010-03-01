ff414942b53e3fedefa3a75795aac44582ae1487
Roger Jakobsen <roger.jakobsen@gmail.com>
2010-03-01 09:57:40 +0100

    Tok bort filer som har blitt flyttet i og med refactoring

--- src/main/java/no/knowit/trafikantenkiller/Routeplanner.java
+++
@@ -1,9 +0,0 @@
-package no.knowit.trafikantenkiller;
-
-import no.knowit.trafikantenkiller.model.nodes.Station;
-
-public interface Routeplanner {
-
-	public Route planRoute(Station from, Station to);
-
-}
